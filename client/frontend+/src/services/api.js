683914da09601d4a1bbf4754d0e1c3035d53e449
rob587 <robertocamm02@yahoo.com>
2026-05-18 11:47:43 +0200

    feat: add API functions for signal and connection management

--- client/frontend+/src/services/api.js
+++ client/frontend+/src/services/api.js
@@ -4,4 +4,23 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
+export const getSignals = () => {
+  return API.get("/signals/all");
+};
+
+export const createSignal = (data) => {
+  return API.post("/signals/create", data);
+};
+
+export const findConnections = (signalId) => {
+  return API.post("/connections/find", { signal_id: signalId });
+};
+
+export const getConnections = (signalId = null) => {
+  const url = signalId
+    ? `/connections/all?signal_id=${signalId}`
+    : "/connections/all";
+  return API.get(url);
+};
+
 export default API;
