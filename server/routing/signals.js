9540d05ce60b7d9c7ff7f97446f0499f0f71ebcc
rob587 <robertocamm02@yahoo.com>
2026-06-16 12:36:32 +0200

    feat: add edit and delete routes for signals in signals router

--- server/routing/signals.js
+++ server/routing/signals.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createSignal, getSignals } = require("../controllers/signalController");
+const {
+  createSignal,
+  getSignals,
+  editSignal,
+  deleteSignal,
+} = require("../controllers/signalController");
 
 console.log("le route stanno caricando..");
 
 router.post("/create", createSignal);
 router.get("/all", getSignals);
+router.delete("/:id", deleteSignal);
+router.put("/:id", editSignal);
 
 module.exports = router;
